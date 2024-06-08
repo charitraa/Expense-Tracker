809ab6220cffd30af0246f00558db4dece7be8aa
charitraa <stharabi9862187405@gmail.com>
2024-06-08 22:57:53 +0545

    fetch the amount from backend

--- Expense-Tracker/frontend/src/module/OverViewComponent.jsx
+++ Expense-Tracker/frontend/src/module/OverViewComponent.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import axios from 'axios'
+
 import {
   AddTransationContainer,
   AddTransation,
@@ -68,12 +70,65 @@ const AddTransitionView = props => {
 }
 
 const OverViewComponent = props => {
+  const [current, setCurrent] = useState(null)
+  const [income, setincome] = useState(null)
+  const [expense, setexpense] = useState(null)
+
+  useEffect(() => {
+    const fetchcurrent = async () => {
+      try {
+        const response = await axios.get(
+          'http://127.0.0.1:8000/current-amount/'
+        )
+        if (response.status !== 200) {
+          console.log('slight server error')
+        }
+        const data = response.data
+        setCurrent(data)
+      } catch (error) {
+        console.error('Error fetching transactions:', error)
+      }
+    }
+    fetchcurrent()
+  }, [])
+  useEffect(() => {
+    const fetchincome = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/total-income/')
+        if (response.status !== 200) {
+          console.log('slight server error')
+        }
+        const data = response.data
+        setincome(data)
+      } catch (error) {
+        console.error('Error fetching transactions:', error)
+      }
+    }
+    fetchincome()
+  }, [])
+
+  useEffect(() => {
+    const fetchexpense = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/total-expense/')
+        if (response.status !== 200) {
+          console.log('slight server error')
+        }
+        const data = response.data
+        setexpense(data)
+      } catch (error) {
+        console.error('Error fetching transactions:', error)
+      }
+    }
+    fetchexpense()
+  }, [])
+
   const [isAddTxnVisible, toggleAddTxn] = useState(false)
   return (
     <>
       <Container>
         <BalanceBox>
-          Balance: $1000
+          Balance: Rs. {current ? current.current_amount : 'Loading...'}
           <AddTransation onClick={() => toggleAddTxn(!isAddTxnVisible)}>
             {isAddTxnVisible ? 'Cancel' : 'Add'}
           </AddTransation>
@@ -87,10 +142,11 @@ const OverViewComponent = props => {
         )}
         <ExpenseConatiner>
           <ExpenseBox>
-            Expense<span>$1000</span>
+            Expense
+            <span>Rs. {expense ? expense.total_expense : 'Loading...'}</span>
           </ExpenseBox>
           <IncomeBox>
-            Income<span>$10000</span>
+            Income<span>Rs. {income ? income.total_income : 'Loading...'}</span>
           </IncomeBox>
         </ExpenseConatiner>
       </Container>
